e22e8a732b470712def5baa2ceab71028575f4c2
Jay <jpnappy@gmail.com>
2016-03-27 10:45:14 -0400

    my first commit

---
+++ morning-exercise.rb
@@ -0,0 +1,12 @@
+# declare a variable called "favorite_number"
+# set it equal to your favorite number
+favorite_number = 7
+
+# declare a variable called "second_favorite_number"
+# set it equal to your second favorite number
+second_favorite_number = 8
+
+# ask whether "favorite_number" is greater than
+#  "second_favorite_number" and print the result to the
+# screen
+puts(favorite_number > second_favorite_number)
---
+++ second-morning-exercise.rb
@@ -0,0 +1,8 @@
+my_name = "Jay"
+my_age = 28
+# this is a comment
+def my_description(name, age)
+  puts("My name is #{name} and I am #{age} years old")
+end
+# this is another comment
+my_description(my_name, my_age)
